fc4aa376b560b120ba42197261688b1e71ebd106
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-10-19 20:31:08 -0300

    add new api content

---
+++ src/modules/input.js
@@ -0,0 +1,19 @@
+document.getElementById('submit-button').addEventListener('click', () => {
+  const input1 = document.querySelector('#input1');
+  const input2 = document.querySelector('#input2');
+
+  const data = {
+    user: input1.value,
+    score: input2.value,
+  };
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/77714/scores/', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+  console.log('papita');
+});
